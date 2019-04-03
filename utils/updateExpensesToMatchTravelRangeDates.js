74374cccb0849815f88d3fb3ec73efa1f0185575
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2019-04-03 11:03:46 +0200

    Refactor updateExpensesToMatchTravelRangeDates , add convertRateToHomeCurrencyRate and recalculate amountConverted for Expense

--- utils/updateExpensesToMatchTravelRangeDates.js
+++ utils/updateExpensesToMatchTravelRangeDates.js
@@ -6,34 +6,7 @@ const Rate = require('../models/Rate');
 const Currency = require('../models/Currency');
 
 const findRatesByExactOrClosestDate = require('./findRatesByExactOrClosestDate');
-
-const checkCurrenciesArray = (dateFrom, dateTo, travelCurrencies, expenses, travel) => {
-  const newObject = {};
-  let trCurKeys = Object.keys(travelCurrencies);
-
-  for (let d = dateFrom; d <= dateTo; d.add(1, 'days')) {
-    const day = moment(d).format('YYYY-MM-DD');
-    let inArayTest = trCurKeys.includes(day);
-    if (!inArayTest) {
-      // console.log(inArayTest);
-      newObject[day] = [];
-    } else {
-      // console.log(inArayTest);
-      newObject[day] = travelCurrencies[day]
-    }
-  }
-  trCurKeys = Object.keys(newObject);
-  expenses.forEach((expense) => {
-    const day = moment(d).format('YYYY-MM-DD');
-    const cur = expense.invoiceCurrency;
-    let rates = Rate.findRatesOnDate(getTravels);
-    if (rates.length === 0) {
-      rates = Rate.findRateBeforeOrAfterDate(travel)
-    }
-  });
-
-  return newObject;
-}
+const convertRateToHomeCurrencyRate = require('./convertRateToHomeCurrencyRate');
 
 function checkExpenseDate(expDate, travelDateFrom, travelDateTo) {
   console.log('checkExpenseDate', expDate.toISOString(), travelDateFrom.toISOString(), travelDateTo.toISOString());
@@ -67,85 +40,44 @@ module.exports = async (travel, rates) => {
       let invoiceDate = new Date(iDate);
       Currency.find({date: expense.date}, async (err, curRates) => {
 
-        const x = curRates.filter((item) => {
+        const filertedRatesFromDB = curRates.filter((item) => {
           return !isNaN(item.rate[invoiceCurrency]);
-        })
-        // console.log('x', x);
-        if (x.length === 0) {
-          let cur = {};
+        });
 
-          // const condition = {$or: [
-          //   {date: invoiceDate},
-          //   {date: {$gte: invoiceDate}},
-          //   {date: {$lte: invoiceDate}}
-          // ]};
+        if (filertedRatesFromDB.length === 0) {
+          let cur = {};
+          console.log('No rate for this date and currency in database yet');
 
           const dateRates = await findRatesByExactOrClosestDate(invoiceDate);
-          console.log('dateRates date', dateRates.date.toISOString());
-          cur[invoiceCurrency] = Number((dateRates.rates[invoiceCurrency]).toFixed(2));
+          const convertedRate = await convertRateToHomeCurrencyRate(dateRates.rates, travel.homeCurrency, invoiceCurrency);
+          cur[invoiceCurrency] = convertedRate;
+
           const curRate = new Currency({
             base: travelHomeCurrency,
             date: expense.date,
             rate: cur
           });
           await curRate.save();
-          expense.curRate = curRate._id
+          const amountConverted = Number((expense.amount / convertedRate).toFixed(2));
           await Expense.findByIdAndUpdate(expense._id, {
             $set: {
               date: expense.date,
-              curRate: curRate._id
+              curRate: curRate._id,
+              amountConverted: amountConverted
             }
           })
-
-          // await Rate.findOne(condition, async (err, item) => {
-          //   // console.log(item);
-          //   cur[invoiceCurrency] = item.rates[invoiceCurrency];
-          //   // console.log(cur);
-          //   const curRate = new Currency({
-          //     base: travelHomeCurrency,
-          //     date: expense.date,
-          //     rate: cur
-          //   });
-          //   await curRate.save();
-          //   expense.curRate = curRate._id
-          //   await Expense.findByIdAndUpdate(expense._id, {
-          //     $set: {
-          //       date: expense.date,
-          //       curRate: curRate._id
-          //     }
-          //   })
-          // }).sort({date: 0});
         } else {
+          const convertedRate = filertedRatesFromDB[0].rate[invoiceCurrency];
+          const amountConverted = Number((expense.amount / convertedRate).toFixed(2));
           await Expense.findByIdAndUpdate(expense._id, {
             $set: {
               date: expense.date,
-              curRate: x[0]._id
+              curRate: filertedRatesFromDB[0]._id,
+              amountConverted: amountConverted
             }
           })
         }
       });
     }
-
-    // let invoiceCurrency = Object.keys(expense.curRate.rate)[0];
-    // console.log('invoiceCurrency', invoiceCurrency);
-    // Currency.find({date: expense.date}, (err, curRates) => {
-    //
-    //   const x = curRates.filter((item) => {
-    //     return !isNaN(item.rate[invoiceCurrency]);
-    //   })
-    //   if (x.length === 0) {
-    //     let curRate = {};
-    //     Rate.find({date: expense.date}, (item) => {
-    //       curRate = item.rates[invoiceCurrency];
-    //       expense.curRate = curRate;
-    //     })
-    //   }
-    // });
-
-
-
-
   })
-  // const newObject = checkCurrenciesArray(dateFrom, dateTo, travelCurrencies, expenses, travel);
-  // const updatedTravel = await Travel.findByIdAndUpdate({_id: travel._id}, {$set: {travelCurrencies: newObject}}, {new: true})
 }

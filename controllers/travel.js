918011135e65dad35834f4e4d9cf8c1466bc44e2
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2019-04-08 15:32:40 +0200

    route post /import working

--- controllers/travel.js
+++ controllers/travel.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const _ = require('lodash');
 const moment = require('moment');
+const fs = require('fs');
+const Papa = require('papaparse');
 
 const User = require('../models/User');
 const Travel = require('../models/Travel');
@@ -11,7 +13,7 @@ const ObjectId = mongoose.Types.ObjectId;
 const {expenseTypes} = require('../lib/globals');
 const constants = require('../lib/constants');
 
-const updateExpensesToMatchTravelRangeDates  = require('../utils/updateExpensesToMatchTravelRangeDates');
+const updateExpensesToMatchTravelRangeDates = require('../utils/updateExpensesToMatchTravelRangeDates');
 
 exports.getImport = async function(req, res, next) {
   res.render('travels/import', {
@@ -19,15 +21,94 @@ exports.getImport = async function(req, res, next) {
   })
 }
 
+async function checkFile(condition, message) {
+  const suffix = 'File should be a CSV with header in first line and not empty!'
+  if (condition) {
+    throw new Error(`${message} - ${suffix}`);
+  }
+}
+
+
+async function readAndParseFile(filePath, enc='utf8') {
+  try {
+    const myFile = fs.readFileSync(filePath, enc);
+    const parsedData = Papa.parse(myFile, {
+      quoteChar: '"',
+      escapeChar: '"',
+      header: true,
+      dynamicTyping: false,
+      preview: 0,
+      encoding: "utf8",
+      complete: (results) => {
+      },
+      skipEmptyLines: true
+    });
+    return parsedData;
+  } catch (err) {
+    throw err;
+  }
+}
+
+function deleteFile(filePath, message='') {
+  try {
+    if (fs.existsSync(filePath)) {
+      fs.unlink(filePath, (err) => {
+        if (err) {
+          throw err;
+        }
+        console.log(message);
+      });
+    }
+  } catch (err) {
+    console.log(`File: ${filePath} not deleted!`);
+    throw err;
+  }
+}
+
+
 exports.postImport = async function(req, res, next) {
-  const myFile = req.files.myFile;
-  res.redirect('/import')
+  let message = '';
+  const travelHeaderArray = constants.TRAVEL_HEADER;
+  const myFilePath = req.files.myFile.path;
+  try {
+    // check if file is selected, not empty and CSV
+    await checkFile(req.files.myFile.name === '', 'No file selected!');
+    await checkFile(req.files.myFile.size === 0, 'Empty file!');
+    await checkFile(myFilePath.split('.').pop() != 'csv', 'Not a CSV file!');
+    const parsedData = await readAndParseFile(myFilePath);
+
+    // check if file has correct header
+    const dataArray = parsedData.data;
+    const parsedHeaderArray = parsedData.meta.fields;
+    await checkFile(!_.isEqual(travelHeaderArray, parsedHeaderArray), `Header should be: ${travelHeaderArray}`);
+    _.forEach(dataArray, (value, key) => {
+      dataArray[key]._user = req.user._id;
+    })
+    const travels = await Travel.insertMany(dataArray);
+    const travelObjectIds = travels.map(travel => travel._id);
+    await User.findByIdAndUpdate(req.user._id, {
+      $addToSet: {'travels':{ $each: travelObjectIds} }
+    });
+
+    deleteFile(myFilePath, 'File deleted after processed!');
+    message = `${travelObjectIds.length} travels added successfully!`
+    req.flash('success', {msg: message});
+    res.redirect('/travels')
+
+  } catch (err) {
+    deleteFile(myFilePath, 'File deleted after error!');
+    req.flash('errors', {msg: err.message})
+    console.log(err);
+    res.redirect('/import');
+  }
 }
 
 // get all travels
 exports.getTravels = async function(req, res) {
   const travels = await Travel.find({
-    _id: {$in: req.user.travels}
+    _id: {
+      $in: req.user.travels
+    }
   });
   res.render('travels/travels', {
     title: 'Travels',
@@ -52,10 +133,18 @@ exports.getNewTravel = async function(req, res) {
 // create new travel
 exports.postNewTravel = async function(req, res, next) {
 
-  req.assert('description', 'Description is empty or to long (max 60 characters)!').isLength({min: 1, max: 60});
-  req.assert('homeCurrency', 'Home currency should have exactly 3 characters!').isLength({min: 3, max: 3});
+  req.assert('description', 'Description is empty or to long (max 60 characters)!').isLength({
+    min: 1,
+    max: 60
+  });
+  req.assert('homeCurrency', 'Home currency should have exactly 3 characters!').isLength({
+    min: 3,
+    max: 3
+  });
 
-  const decimalOptions = {decimal_digits: 2};
+  const decimalOptions = {
+    decimal_digits: 2
+  };
   req.assert('perMileAmount', 'Per mile amount should be positive number with 2 decimals!').isDecimal(decimalOptions);
 
   const dateCompare = moment(req.body.dateTo).add(1, 'days').format('YYYY-MM-DD');
@@ -94,11 +183,13 @@ exports.postNewTravel = async function(req, res, next) {
     return next(err);
   }
 
-  req.flash('success', {msg: 'Successfully added new travel!'});
+  req.flash('success', {
+    msg: 'Successfully added new travel!'
+  });
   res.redirect('/travels');
 }
 
-exports.getTravel = async function (req, res, next) {
+exports.getTravel = async function(req, res, next) {
   const id = req.params.id;
 
   if (!ObjectId.isValid(id)) {
@@ -113,7 +204,7 @@ exports.getTravel = async function (req, res, next) {
 
     expenses.forEach((expense, index, arr) => {
       if (expense.type != 'Mileage') {
-        const rate= Object.values(expense.curRate.rate)[0];
+        const rate = Object.values(expense.curRate.rate)[0];
         expense.rate = rate.toFixed(2);
       } else {
         expense.rate = travel.perMileAmount;
@@ -137,7 +228,7 @@ exports.getTravel = async function (req, res, next) {
   }
 };
 
-exports.deleteTravel = async function (req, res, next) {
+exports.deleteTravel = async function(req, res, next) {
   const id = req.params.id;
   // console.log(id);
 
@@ -152,32 +243,50 @@ exports.deleteTravel = async function (req, res, next) {
     });
 
     if (!travel) {
-      req.flash('error', {msg: 'Travel not found!!'});
+      req.flash('error', {
+        msg: 'Travel not found!!'
+      });
       return next(new Error('Travel not found'));
     }
 
-    Expense.deleteMany({travel: travel._id, _user: req.user._id}, (err) => {
-      if (err) {return next(err)}
+    Expense.deleteMany({
+      travel: travel._id,
+      _user: req.user._id
+    }, (err) => {
+      if (err) {
+        return next(err)
+      }
     });
 
-    User.findByIdAndUpdate(req.user._id, {$pullAll : {'travels': [travel._id]}}, (err, user) => {
+    User.findByIdAndUpdate(req.user._id, {
+      $pullAll: {
+        'travels': [travel._id]
+      }
+    }, (err, user) => {
       if (!err) {
         return next(err);
       }
     });
-    req.flash('success', {msg: 'Travel successfully deleted!'});
+    req.flash('success', {
+      msg: 'Travel successfully deleted!'
+    });
     res.redirect('/travels');
   } catch (err) {
     return next(err);
   }
 };
 
-exports.updateTravel = async function (req, res, next) {
+exports.updateTravel = async function(req, res, next) {
 
-  req.assert('description', 'Description is empty or to long (max 120 characters)!').isLength({min: 1, max: 60});
-  req.assert('homeCurrency', 'Home currency should have exactly 3 characters!').isLength({min: 3, max: 3});
-  req.assert('perMileAmount', 'Per mile amount should be positive number with 2 decimals!').isNumeric().isCurrency(
-    {
+  req.assert('description', 'Description is empty or to long (max 120 characters)!').isLength({
+    min: 1,
+    max: 60
+  });
+  req.assert('homeCurrency', 'Home currency should have exactly 3 characters!').isLength({
+    min: 3,
+    max: 3
+  });
+  req.assert('perMileAmount', 'Per mile amount should be positive number with 2 decimals!').isNumeric().isCurrency({
     allow_negatives: false,
     allow_negative_sign_placeholder: true,
     thousands_separator: ',',
@@ -200,16 +309,25 @@ exports.updateTravel = async function (req, res, next) {
     return res.redirect(`/travels/${id}`);
   }
 
-  const body = _.pick(req.body, ['description', 'dateFrom', 'dateTo','homeCurrency', 'perMileAmount']);
+  const body = _.pick(req.body, ['description', 'dateFrom', 'dateTo', 'homeCurrency', 'perMileAmount']);
 
   if (!ObjectId.isValid(id)) {
     return next(new Error('Not valid Object Id'));
   }
 
   try {
-    let travel = await Travel.findOneAndUpdate({_id: id, _user: req.user.id}, {$set: body}, {new: true}).populate({
+    let travel = await Travel.findOneAndUpdate({
+      _id: id,
+      _user: req.user.id
+    }, {
+      $set: body
+    }, {
+      new: true
+    }).populate({
       path: 'expenses',
-      populate: {path: 'curRate'}
+      populate: {
+        path: 'curRate'
+      }
     });
 
     if (!travel) {
@@ -218,15 +336,22 @@ exports.updateTravel = async function (req, res, next) {
 
     await updateExpensesToMatchTravelRangeDates(travel, res.locals.rates);
     await travel.save();
-    travel = await Travel.findOne({_id: travel._id, _user: req.user.id}).populate({
+    travel = await Travel.findOne({
+      _id: travel._id,
+      _user: req.user.id
+    }).populate({
       path: 'expenses',
-      populate: {path: 'curRate'}
+      populate: {
+        path: 'curRate'
+      }
     });
     const total = await travel.updateTotal();
     travel.total = parseFloat(total).toFixed(2);
     await travel.save();
 
-    req.flash('success', {msg: 'Travel successfully updated!'});
+    req.flash('success', {
+      msg: 'Travel successfully updated!'
+    });
     res.redirect('/travels');
   } catch (err) {
     return next(err);

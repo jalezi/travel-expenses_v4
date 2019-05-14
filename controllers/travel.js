c2df4ada2b97bab93aafea914a79c06f458d3235
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2019-05-14 16:12:15 +0200

    Refactor travel controller

--- controllers/travel.js
+++ controllers/travel.js
@@ -18,174 +18,97 @@ const updateExpensesToMatchTravelRangeDates = require('../utils/updateExpensesTo
 const travelExpensesToPDF = require('../utils/travelExpensesToPDF');
 const travelsTotalToPDF = require('../utils/travelsTotalToPDF');
 
+
+
 /*
-* travelsTotalToPDF
-*/
-function createTravelsTotalPDF(res, travels, user, dateRange, sum) {
-  const stream = travelsTotalToPDF(travels, user, dateRange, sum);
-  let filename = "travelReportDocument.pdf";
-  // Be careful of special characters
-
-  filename = encodeURIComponent(filename);
-  // Ideally this should strip them
-
-  res.setHeader('Content-disposition', 'inline; filename="' + filename + '"');
-  res.setHeader('Content-type', 'application/pdf');
-  // console.log('stream', stream);
-  stream.pipe(res);
-}
-
+ * GET /travels/total_pdf
+ */
 exports.getTravelsTotalPDF = async function(req, res, next) {
+// Create and open PDF
+  function createTravelsTotalPDF(res, travels, user, dateRange, sum) {
+    const stream = travelsTotalToPDF(travels, user, dateRange, sum);
+    let filename = `TOTAL_${user._id}_${df}_${dt}.pdf`;  // Be careful of special characters
+    filename = encodeURIComponent(filename);  // Ideally this should strip them
+    res.setHeader('Content-disposition', 'inline; filename="' + filename + '"');
+    res.setHeader('Content-type', 'application/pdf');
+    stream.pipe(res);
+  }
 
-  let travels;
-  let queryDateFrom;
-  let queryDateTo;
-  let totalSum;
+  let travels, queryDateFrom, queryDateTo, totalSum;
+  // Get date range from url query
   const df = req.query.df;
   const dt = req.query.dt;
-  const dateRange = {
-    df,
-    dt
-  };
+  const dateRange = {df, dt};
 
+  // TODO if statement is safety in case date range is not passed as url query
   if (df === '' || dt === '') {
     travels = await Travel.find({_user: res.locals.user._id});
     totalSum = Travel.aggregate([
-  {
-    '$match': {
-      '_user': res.locals.user._id
-    }
-  }, {
-    '$group': {
-      '_id': null,
-      'sum': {
-        '$sum': '$total'
-      }
-    }
-  }
-], (err, result) => {
-  if (err) {
-    next(err)
-  } else {
-    let sum;
-    if (result.length === 0) {
-      sum = 0;
+      {'$match': {'_user': res.locals.user._id}},
+      {'$group': {'_id': null, 'sum': {'$sum': '$total'}}}
+    ], (err, result) => {
+    if (err) {next(err)
     } else {
-      sum = Number(result[0].sum);
+      let sum;
+      if (result.length === 0) {sum = 0;} else {sum = Number(result[0].sum);}
+      createTravelsTotalPDF(res, travels, res.locals.user, dateRange, sum);
     }
-    createTravelsTotalPDF(res, travels, res.locals.user, dateRange, sum);
-  }
-
 });
-
   } else {
     queryDateFrom = new Date(df);
     queryDateTo = new Date(dt);
     travels = await Travel.find({
       _user: res.locals.user._id,
-      $and: [
-        {dateFrom: {$gte: queryDateFrom}}, {dateFrom: {$lte: queryDateTo}}
-      ]
+      $and: [{dateFrom: {$gte: queryDateFrom}}, {dateFrom: {$lte: queryDateTo}}]
     });
-
     totalSum = Travel.aggregate([
-  {
-    '$match': {
-      '_user': res.locals.user._id,
-      $and: [
-        {dateFrom: {$gte: queryDateFrom}}, {dateFrom: {$lte: queryDateTo}}
-      ]
-    }
-  }, {
-    '$group': {
-      '_id': null,
-      'sum': {
-        '$sum': '$total'
-      }
-    }
-  }
+      {'$match': {'_user': res.locals.user._id, $and: [{dateFrom: {$gte: queryDateFrom}}, {dateFrom: {$lte: queryDateTo}}]}},
+      {'$group': {'_id': null, 'sum': {'$sum': '$total'}}}
 ], (err, result) => {
   if (err) {
     next(err)
   } else {
     let sum;
-    if (result.length === 0) {
-      sum = 0;
-    } else {
-      sum = Number(result[0].sum);
-    }
+    if (result.length === 0) {sum = 0;} else {sum = Number(result[0].sum);}
     createTravelsTotalPDF(res, travels, res.locals.user, dateRange, sum);
   }
-
-});
-  }
-}
+});}}
 
 /*
-  travelExpensesToPDF
-*/
-
+ * GET /travels/:id/pdf
+ */
 exports.getTravelExpensesPDF = async function(req, res, next) {
-
   const stream = travelExpensesToPDF(res.locals.travel, res.locals.user);
-  let filename = "travelReportDocument.pdf";
-  // Be careful of special characters
-
-  filename = encodeURIComponent(filename);
-  // Ideally this should strip them
-
+  let filename = "travelReportDocument.pdf";  // Be careful of special characters
+  filename = encodeURIComponent(filename);  // Ideally this should strip them
   res.setHeader('Content-disposition', 'inline; filename="' + filename + '"');
   res.setHeader('Content-type', 'application/pdf');
-  // console.log('stream', stream);
   stream.pipe(res);
 }
 
-
-
-// get all travels
+/*
+ * GET /travels
+ */
 exports.getTravels = async function(req, res) {
   let minDate;
   let maxDate;
   let yearMin;
   let yearMax;
   let years = [];
-  const travels = await Travel.find({
-    _id: {
-      $in: req.user.travels
-    }
-  }).sort('-dateFrom');
-  /*
-  * TODO create new Promise
-  */
-  const minMaxTravelsDates = await Travel.aggregate([
-  {
-    '$match': {
-      '_user': req.user._id
-    }
-  }, {
-    '$group': {
-      '_id': req.user._id,
-      'minDate': {
-        '$min': '$dateFrom'
-      },
-      'maxDate': {
-        '$max': '$dateFrom'
-      }
-    }
-  }
-], (err, doc) => {
-  if (err) {
-    next(err);
-  }
+  const travels = await Travel.find({_id: {$in: req.user.travels}}).sort('-dateFrom');
 
+  // TODO create new Promise
+  const minMaxTravelsDates = await Travel.aggregate([
+    {'$match': {'_user': req.user._id}},
+    {'$group': {'_id': req.user._id,'minDate': {'$min': '$dateFrom'}, 'maxDate': {'$max': '$dateFrom'}}}
+], (err, doc) => {
+  if (err) {next(err);}
   if (doc.length != 0) {
     minDate = moment(doc[0].minDate).format('YYYY-MM-DD');
     maxDate = moment(doc[0].maxDate).format('YYYY-MM-DD');
     yearMin = moment(doc[0].minDate).format('YYYY');
     yearMax = moment().format('YYYY');
-    for (i = Number(yearMax); i >= Number(yearMin); i--) {
-      years.push(i);
-    }
+    for (i = Number(yearMax); i >= Number(yearMin); i--) {years.push(i);}
   } else {
     minDate = maxDate = moment().format('YYYY-MM-DD');
     yearMin = yearMax = moment().format('YYYY');
@@ -202,35 +125,25 @@ res.render('travels/travels', {
 });
 }
 
-
-// get all travels
+/*
+ * GET /travels/new
+ */
 exports.getNewTravel = async function(req, res) {
-  // console.log('getNewTravel');
-  // const travels = await Travel.find({
-  //   user: req.user._id
-  // });
   res.render('travels/new', {
     title: 'New travel',
     user: req.user
   });
 }
 
-// post travels
-// create new travel
+/*
+ * POST /travels/new
+ */
 exports.postNewTravel = async function(req, res, next) {
 
-  req.assert('description', 'Description is empty or to long (max 60 characters)!').isLength({
-    min: 1,
-    max: 60
-  });
-  req.assert('homeCurrency', 'Home currency should have exactly 3 characters!').isLength({
-    min: 3,
-    max: 3
-  });
+  req.assert('description', 'Description is empty or to long (max 60 characters)!').isLength({min: 1, max: 60});
+  req.assert('homeCurrency', 'Home currency should have exactly 3 characters!').isLength({min: 3,max: 3});
 
-  const decimalOptions = {
-    decimal_digits: 2
-  };
+  const decimalOptions = {decimal_digits: 2};
   req.assert('perMileAmount', 'Per mile amount should be positive number with 2 decimals!').isDecimal(decimalOptions);
 
   const dateCompare = moment(req.body.dateTo).add(1, 'days').format('YYYY-MM-DD');
@@ -256,34 +169,20 @@ exports.postNewTravel = async function(req, res, next) {
 
   try {
     const doc = await travel.save();
-    await User.findByIdAndUpdate(req.user._id, {
-      $addToSet: {
-        'travels': doc._id
-      },
-    }, (err, user) => {
-      if (err) {
-        return next(err);
-      }
-    });
-  } catch (err) {
-    return next(err);
-  }
+    await User.findByIdAndUpdate(req.user._id, {$addToSet: {'travels': doc._id}}, (err, user) => {if (err) {return next(err);}});
+  } catch (err) {return next(err);}
 
-  req.flash('success', {
-    msg: 'Successfully added new travel!'
-  });
+  req.flash('success', {msg: 'Successfully added new travel!'});
   res.redirect('/travels');
 }
 
+/*
+ * GET /travels/:id
+ */
 exports.getTravel = async function(req, res, next) {
   const id = req.params.id;
-
-  if (!ObjectId.isValid(id)) {
-    return next(new Error('Not valid Object Id'));
-  }
-
+  if (!ObjectId.isValid(id)) {  return next(new Error('Not valid Object Id'));}
   const travel = res.locals.travel;
-
 
   try {
     const expenses = travel.expenses;
@@ -292,14 +191,10 @@ exports.getTravel = async function(req, res, next) {
       if (expense.type != 'Mileage') {
         const rate = Object.values(expense.curRate.rate)[0];
         expense.rate = rate.toFixed(2);
-      } else {
-        expense.rate = travel.perMileAmount;
-      }
+      } else {expense.rate = travel.perMileAmount;}
     });
 
-    if (!travel) {
-      return next(new Error('Travel not found'))
-    }
+    if (!travel) {return next(new Error('Travel not found'))}
 
     res.render('travels/travel', {
       title: 'Travel',
@@ -309,70 +204,43 @@ exports.getTravel = async function(req, res, next) {
       constants,
       rates: JSON.stringify(res.locals.rates)
     });
-  } catch (err) {
-    return next(err);
-  }
+  } catch (err) {return next(err);}
 };
 
+/*
+ * DELETE /travels/:id
+ */
 exports.deleteTravel = async function(req, res, next) {
   const id = req.params.id;
-  // console.log(id);
 
-  if (!ObjectId.isValid(id)) {
-    return next(new Error('Not valid Object Id'));
-  }
+  if (!ObjectId.isValid(id)) {return next(new Error('Not valid Object Id'));}
 
   try {
-    const travel = await Travel.findOneAndDelete({
-      _id: id,
-      _user: req.user._id
-    });
+    const travel = await Travel.findOneAndDelete({_id: id, _user: req.user._id});
 
     if (!travel) {
-      req.flash('error', {
-        msg: 'Travel not found!!'
-      });
+      req.flash('error', {msg: 'Travel not found!!'});
       return next(new Error('Travel not found'));
     }
 
-    Expense.deleteMany({
-      travel: travel._id,
-      _user: req.user._id
-    }, (err) => {
-      if (err) {
-        return next(err)
-      }
+    Expense.deleteMany({travel: travel._id, _user: req.user._id}, (err) => {
+      if (err) {return next(err)}
     });
 
-    User.findByIdAndUpdate(req.user._id, {
-      $pullAll: {
-        'travels': [travel._id]
-      }
-    }, (err, user) => {
-      if (!err) {
-        return next(err);
-      }
+    User.findByIdAndUpdate(req.user._id, {$pullAll: {'travels': [travel._id]}}, (err, user) => {
+      if (!err) {return next(err);}
     });
-    req.flash('success', {
-      msg: 'Travel successfully deleted!'
-    });
+    req.flash('success', {msg: 'Travel successfully deleted!'});
     res.redirect('/travels');
-  } catch (err) {
-    return next(err);
-  }
+  } catch (err) {return next(err);}
 };
 
+/*
+ * PATCH /travels/new
+ * TODO fix total calculation
+ */
 exports.updateTravel = async function(req, res, next) {
-
-  req.assert('description', 'Description is empty or to long (max 120 characters)!').isLength({
-    min: 1,
-    max: 60
-  });
-  req.assert('homeCurrency', 'Home currency should have exactly 3 characters!').isLength({
-    min: 3,
-    max: 3
-  });
-  req.assert('perMileAmount', 'Per mile amount should be positive number with 2 decimals!').isNumeric().isCurrency({
+  const currencyOptions = {
     allow_negatives: false,
     allow_negative_sign_placeholder: true,
     thousands_separator: ',',
@@ -381,8 +249,10 @@ exports.updateTravel = async function(req, res, next) {
     require_decimal: false,
     digits_after_decimal: [2],
     allow_space_after_digits: false
-  });
-
+  }
+  req.assert('description', 'Description is empty or to long (max 120 characters)!').isLength({min: 1, max: 60});
+  req.assert('homeCurrency', 'Home currency should have exactly 3 characters!').isLength({min: 3, max: 3});
+  req.assert('perMileAmount', 'Per mile amount should be positive number with 2 decimals!').isNumeric().isCurrency(currencyOptions);
 
   const dateCompare = moment(req.body.dateTo).add(1, 'days').format('YYYY-MM-DD');
   req.assert('dateFrom', 'Date from should be before date to').isBefore(dateCompare);
@@ -397,9 +267,7 @@ exports.updateTravel = async function(req, res, next) {
 
   const body = _.pick(req.body, ['description', 'dateFrom', 'dateTo', 'homeCurrency', 'perMileAmount']);
 
-  if (!ObjectId.isValid(id)) {
-    return next(new Error('Not valid Object Id'));
-  }
+  if (!ObjectId.isValid(id)) {return next(new Error('Not valid Object Id'));}
 
   try {
     let travel = await Travel.findOneAndUpdate({
@@ -416,9 +284,7 @@ exports.updateTravel = async function(req, res, next) {
       }
     });
 
-    if (!travel) {
-      return next(new Error('Travel not found'));
-    }
+    if (!travel) {return next(new Error('Travel not found'));}
 
     await updateExpensesToMatchTravelRangeDates(travel, res.locals.rates);
     travel = await travel.save();
@@ -436,9 +302,7 @@ exports.updateTravel = async function(req, res, next) {
     travel.total = parseFloat(total).toFixed(2);
     travel = await travel.save();
 
-    req.flash('success', {
-      msg: 'Travel successfully updated!'
-    });
+    req.flash('success', {msg: 'Travel successfully updated!'});
     res.redirect('/travels');
   } catch (err) {
     return next(err);

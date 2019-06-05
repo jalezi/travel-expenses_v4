9ca2e68b869737092d3f758c30c03635523c3c57
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2019-06-05 17:14:48 +0200

    refactor test app

--- test/app.js
+++ test/app.js
@@ -1,19 +1,9 @@
 const request = require('supertest');
 const app = require('../app.js');
 
-const { expect } = require('chai');
-const sinon = require('sinon');
-require('sinon-mongoose');
-
 const User = require('../models/User');
-const Travel = require('../models/Travel');
 
-const UserMock = sinon.mock(new User({ email: 'test@gmail.com', password: 'root', travels: []}));
-const user = UserMock.object;
-const TravelMock = sinon.mock(new Travel({_user: user._id, description: 'travel1', dateFrom: new Date('2019-05-01'), dateTo: new Date('2019-05-1'), perMileAmount: user.perMileAmount}));
-const travel = TravelMock.object;
-console.log(user);
-console.log(travel);
+const user = new User({ email: 'test@gmail.com', password: 'root', travels: []});
 
 describe('GET /', () => {
   it('should return 200 OK', (done) => {

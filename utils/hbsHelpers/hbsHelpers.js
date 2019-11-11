e5822f2e4e4b46b7a87a37ccc4b73c521218756c
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2019-11-11 09:45:34 +0100

    refactor hbsHelper.js

--- utils/hbsHelpers/hbsHelpers.js
+++ utils/hbsHelpers/hbsHelpers.js
@@ -1,6 +1,6 @@
 const expressHbs = require('express-hbs');
 const moment = require('moment');
-const createElement = require('../utils').createElement;
+// const createElement = require('../utils').createElement;
 
 expressHbs.registerHelper('flash', (message) => {
   if (message.error) {
@@ -12,114 +12,97 @@ expressHbs.registerHelper('flash', (message) => {
   if (message.success) {
     return message.success;
   }
-
-})
+});
 
 // expressHbs.registerHelper('gravatar', (user) => {
 //   return user.gravatar(60);
 // })
 
-expressHbs.registerHelper('debug', function(data, breakpoint) {
+expressHbs.registerHelper('debug', (data) => {
   console.log(data);
-  if (breakpoint === true) {
-    debugger;
+  // if (breakpoint === true) {
+  //   debugger;
+  // }
+});
+
+expressHbs.registerHelper('gender', (userGender, radioButtonGender) => userGender === radioButtonGender);
+
+expressHbs.registerHelper('setChecked', (value, currentValue) => {
+  if (value === currentValue) {
+    return 'checked';
+  }
+
+  return '';
+});
+
+expressHbs.registerHelper('setOption', (value, currentValue) => {
+  if (value === currentValue) {
+    return "selected='selected'";
   }
   return '';
 });
 
-expressHbs.registerHelper('gender', (userGender, radioButtonGender) => {
-  return userGender == radioButtonGender;
-})
+expressHbs.registerHelper('setValue', (value) => `value=${value}`);
 
-expressHbs.registerHelper ("setChecked", function (value, currentValue) {
-    if ( value == currentValue ) {
-       return "checked";
-    } else {
-       return "";
-    }
- });
+expressHbs.registerHelper('countList', (value) => value + 1);
 
- expressHbs.registerHelper("setOption", (value, currentValue) => {
-   if (value == currentValue) {
-     return "selected='selected'";
-   } else {
-     return;
-   }
- })
+expressHbs.registerHelper('formatDate', (date) => {
+  if (!date) {
+    const today = moment().format('YYYY-MM-DD');
+    return today;
+  }
+  const today = moment(date).format('YYYY-MM-DD');
+  return today;
+});
 
- expressHbs.registerHelper('setValue', (value) => {
-   return `value=${value}`;
- })
+expressHbs.registerHelper('formatMonth', (date) => {
+  if (!date) {
+    const today = moment().format('MMMM, YYYY');
+    return today;
+  }
+  const today = moment(date).format('MMMM, YYYY');
+  return today;
+});
 
- expressHbs.registerHelper('countList', (value) => {
-   return value + 1;
- })
+expressHbs.registerHelper('travelsList', (items, options) => {
+  let out = '<ul>';
 
- expressHbs.registerHelper('formatDate', (date) => {
-   if (!date) {
-     const today = moment().format('YYYY-MM-DD');
-     return today;
-   }
-   else {
-     const today = moment(date).format('YYYY-MM-DD');
-     return today;
-   }
- });
-
- expressHbs.registerHelper('formatMonth', (date) => {
-   if (!date) {
-     const today = moment().format('MMMM, YYYY');
-     return today;
-   }
-   else {
-     const today = moment(date).format('MMMM, YYYY');
-     return today;
-   }
- })
-
- expressHbs.registerHelper('travelsList', function(items, options) {
-  let out = "<ul>";
-
-  for(let i=0, length=items.length; i<length; i++) {
-    out = out + "<li>" + options.fn(items[i]) + "</li>";
+  // eslint-disable-next-line prefer-destructuring
+  for (let i = 0, length = items.length; i < length; i++) {
+    out = `${out}<li>${options.fn(items[i])}</li>`;
   }
 
-  return out + "</ul>";
+  return `${out}</ul>`;
 });
 
 expressHbs.registerHelper('setUnit', (homeDistance) => {
   if (homeDistance === 'mi') {
-      return 'mile';
-    } else if (homeDistance === 'km') {
-      return 'km';
-    } else {
-      return '';
-    }
+    return 'mile';
+  }
+  return 'km';
 });
 
 expressHbs.registerHelper('setUnit2', (homeDistance) => {
-  if (homeDistance != 'mi') {
-      return 'mile';
-    } else if (homeDistance != 'km') {
-      return 'km';
-    } else {
-      return '';
-    }
+  if (homeDistance !== 'mi') {
+    return 'mile';
+  } if (homeDistance !== 'km') {
+    return 'km';
+  }
+  return '';
 });
 
-expressHbs.registerHelper('toNumber' , (valueAsString) => {
-  return parseFloat(valueAsString);
-});
+expressHbs.registerHelper('toNumber', (valueAsString) => parseFloat(valueAsString));
 
 expressHbs.registerHelper('getRate', (travelCurrencies, currency) => {
-  const item = travelCurrencies.find((item) => {
-    return item.currency.name === currency;
-  });
+  const item = travelCurrencies.find((item) => item.currency.name === currency);
   return item.value;
 });
 
 expressHbs.registerHelper('toCurrency', (number) => {
-  const formatter = new Intl.NumberFormat('en-US', {minimumFractionDigits: 2, maximumFractionDigits: 2});
-  let numberString = formatter.format(number);
+  const formatter = new Intl.NumberFormat('en-US', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+  });
+  const numberString = formatter.format(number);
   return numberString;
 });

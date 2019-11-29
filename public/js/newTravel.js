f83b5836288b4d9b2b4d752e0e52c00a46422137
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2019-11-29 14:35:14 +0100

    prettier and eslint refactoring

--- public/js/newTravel.js
+++ public/js/newTravel.js
@@ -1,21 +1,18 @@
+/* eslint-disable no-undef */
 /* eslint-env jquery, browser */
 
 
 $(document).ready(() => {
-
   dateFrom.onfocusout = () => {
     if (document.getElementById('dateFrom').value > document.getElementById('dateTo').value) {
       document.getElementById('dateTo').value = document.getElementById('dateFrom').value;
-    } else {
-
     }
     document.getElementById('dateTo').min = document.getElementById('dateFrom').value;
-  }
-
+  };
   dateTo.onfocusout = () => {
     if (document.getElementById('dateFrom').value > document.getElementById('dateTo').value) {
       document.getElementById('dateFrom').value = document.getElementById('dateTo').value;
     }
-  }
-    document.getElementById('dateTo').min = document.getElementById('dateFrom').value;
+  };
+  document.getElementById('dateTo').min = document.getElementById('dateFrom').value;
 });
